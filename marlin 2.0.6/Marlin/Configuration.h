81d4f8ff98ae253bb1b4752b9ad0bd487358379d
Aipel3dbcn <77935145+Aipel3dbcn@users.noreply.github.com>
2021-01-25 08:34:06 +0100

    Update Configuration.h

--- marlin 2.0.6/Marlin/Configuration.h
+++ marlin 2.0.6/Marlin/Configuration.h
@@ -759,7 +759,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 400, 816 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 816 } //E3D Titan extruder
 
 /**
  * Default Max Feed Rate (mm/s)
